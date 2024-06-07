bdc663b582712217d81b526df0459c781ea4b267
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-06-08 00:46:01 +0200

    Prosze nie krzycz, to jest work in progress :(

--- app/src/main/java/com/example/watertrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/watertrackerapp/MainActivity.kt
@@ -3,36 +3,17 @@ package com.example.watertrackerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import com.example.watertrackerapp.ui.theme.WaterTrackerAppTheme
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import androidx.compose.runtime.rememberCoroutineScope
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         val uID = intent
         val userID = uID.getStringExtra("uID")
         val userAge = uID.getIntExtra("userAge", 0)
@@ -43,7 +24,7 @@ class MainActivity : ComponentActivity() {
             WaterTrackerAppTheme {
                 Surface {
                     if (userID != null) {
-                        WaterIntakeScreen(userID, userAge, userWeight, userHeight)
+                       // WaterIntakeScreen(userID, userAge, userWeight, userHeight)
                     }
                 }
             }
@@ -51,56 +32,56 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun WaterIntakeScreen(name: String, userAge: Int, userWeight: Double, userHeight: Double) {
-    var waterIntake by remember { mutableStateOf("") }
-    var totalWaterIntake by remember { mutableStateOf(0) }
-    val database = Firebase.firestore
-    val databaseOp = DatabaseOperations(database)
-    val coroutineScope = rememberCoroutineScope()
-    Column(modifier = Modifier
-        .padding(16.dp)
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Greeting(name = "$name, ")
-        OutlinedTextField(
-            value = waterIntake,
-            onValueChange = { waterIntake = it },
-            label = { Text("Dodaj spożycie wody (ml)") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = {
-                totalWaterIntake += waterIntake.toIntOrNull() ?: 0
-                waterIntake = ""
-            }),
-            modifier = Modifier.fillMaxWidth() // Zastosowanie fillMaxWidth, aby pole tekstowe miało szerokość kolumny
-        )
-        Button(
-            onClick = {
-                totalWaterIntake += waterIntake.toIntOrNull() ?: 0
-                waterIntake = ""
-                coroutineScope.launch {
-                    databaseOp.addWaterIntakeForUser(name, totalWaterIntake.toDouble())
-                }
-            },
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .fillMaxWidth() // Zastosowanie fillMaxWidth, aby przycisk miał szerokość kolumny
-        ) {
-            Text("Dodaj")
-        }
-        Text("Całkowite spożycie wody: $totalWaterIntake ml", modifier = Modifier.padding(top = 8.dp))
-        Recomendation(userWeight, modifier = Modifier.padding(top = 8.dp))
-    }
-}
+//@Composable
+//fun WaterIntakeScreen(name: String, userAge: Int, userWeight: Double, userHeight: Double) {
+//    var waterIntake by remember { mutableStateOf("") }
+//    var totalWaterIntake by remember { mutableStateOf(0) }
+//    val database = Firebase.firestore
+//    val databaseOp = DatabaseOperations(database)
+//    val coroutineScope = rememberCoroutineScope()
+//    Column(modifier = Modifier
+//        .padding(16.dp)
+//        .fillMaxWidth()
+//        .fillMaxHeight()) {
+//        Greeting(name = "$name, ")
+//        OutlinedTextField(
+//            value = waterIntake,
+//            onValueChange = { waterIntake = it },
+//            label = { Text("Dodaj spożycie wody (ml)") },
+//            singleLine = true,
+//            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+//            keyboardActions = KeyboardActions(onDone = {
+//                totalWaterIntake += waterIntake.toIntOrNull() ?: 0
+//                waterIntake = ""
+//            }),
+//            modifier = Modifier.fillMaxWidth() // Zastosowanie fillMaxWidth, aby pole tekstowe miało szerokość kolumny
+//        )
+//        Button(
+//            onClick = {
+//                totalWaterIntake += waterIntake.toIntOrNull() ?: 0
+//                waterIntake = ""
+//                coroutineScope.launch {
+//                    databaseOp.addWaterIntakeForUser(name, totalWaterIntake.toDouble())
+//                }
+//            },
+//            modifier = Modifier
+//                .padding(top = 8.dp)
+//                .fillMaxWidth() // Zastosowanie fillMaxWidth, aby przycisk miał szerokość kolumny
+//        ) {
+//            Text("Dodaj")
+//        }
+//        Text("Całkowite spożycie wody: $totalWaterIntake ml", modifier = Modifier.padding(top = 8.dp))
+//        Recomendation(userWeight, modifier = Modifier.padding(top = 8.dp))
+//    }
+//}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
 
 @Composable
