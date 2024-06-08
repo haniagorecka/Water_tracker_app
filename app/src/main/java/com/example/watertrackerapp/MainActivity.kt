5fae4e33d9f996b4d50bfc6e5dd34c27d172bce9
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-06-08 02:27:39 +0200

    Prosze nie krzycz, to jest work in progress (still) :(

--- app/src/main/java/com/example/watertrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/watertrackerapp/MainActivity.kt
@@ -1,30 +1,60 @@
 package com.example.watertrackerapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
+import androidx.appcompat.widget.AppCompatSpinner
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.example.watertrackerapp.ui.theme.WaterTrackerAppTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var DrinkOption: AppCompatSpinner
+    private lateinit var DrinkAmount: EditText
+    private lateinit var AddButton: Button
+    private lateinit var SmallCup: ImageView
+    private lateinit var MediumCup: ImageView
+    private lateinit var BigCup: ImageView
+    private lateinit var Recommended: TextView
+    private lateinit var Consumption: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DrinkOption = findViewById(R.id.DrinkOptions)
+        DrinkAmount= findViewById(R.id.DrinkAmount)
+        AddButton = findViewById(R.id.AddButton)
+        SmallCup = findViewById(R.id.SmallCup)
+        MediumCup = findViewById(R.id.MediumCup)
+        BigCup = findViewById(R.id.BigCup)
+        Recommended = findViewById(R.id.Recommended)
+        Consumption=findViewById(R.id.Consumption)
+
         val uID = intent
         val userID = uID.getStringExtra("uID")
         val userAge = uID.getIntExtra("userAge", 0)
         val userWeight = uID.getDoubleExtra("userWeight", 0.0)
         val userHeight = uID.getDoubleExtra("userHeight", 0.0)
-        //val userID = intent.getStringExtra("userID") ?: "User"
+        Recommended.text = Recomendation(userWeight)
+
+         //val userID = intent.getStringExtra("userID") ?: "User"
         setContent {
-            WaterTrackerAppTheme {
+           WaterTrackerAppTheme {
                 Surface {
                     if (userID != null) {
-                       // WaterIntakeScreen(userID, userAge, userWeight, userHeight)
+                        WaterIntakeScreen(userID, userAge, userWeight, userHeight)
                     }
                 }
             }
@@ -33,12 +63,12 @@ class MainActivity : ComponentActivity() {
 }
 
 //@Composable
-//fun WaterIntakeScreen(name: String, userAge: Int, userWeight: Double, userHeight: Double) {
-//    var waterIntake by remember { mutableStateOf("") }
-//    var totalWaterIntake by remember { mutableStateOf(0) }
-//    val database = Firebase.firestore
-//    val databaseOp = DatabaseOperations(database)
-//    val coroutineScope = rememberCoroutineScope()
+fun WaterIntakeScreen(name: String, userAge: Int, userWeight: Double, userHeight: Double) {
+    var waterIntake by remember { mutableStateOf("") }
+    var totalWaterIntake by remember { mutableStateOf(0) }
+    val database = Firebase.firestore
+    val databaseOp = DatabaseOperations(database)
+    val coroutineScope = rememberCoroutineScope()
 //    Column(modifier = Modifier
 //        .padding(16.dp)
 //        .fillMaxWidth()
@@ -73,7 +103,7 @@ class MainActivity : ComponentActivity() {
 //        Text("Całkowite spożycie wody: $totalWaterIntake ml", modifier = Modifier.padding(top = 8.dp))
 //        Recomendation(userWeight, modifier = Modifier.padding(top = 8.dp))
 //    }
-//}
+}
 
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -90,6 +120,9 @@ fun Recomendation(weight: Double, modifier: Modifier = Modifier) {
         text = "Rekomendowane spożycie wody: ${(weight * 35.0).toInt()} ml",
         modifier = modifier
     )
+}
+fun Recomendation(weight: Double): String {
+     return "Rekomendowane spożycie wody: ${(weight * 35.0).toInt()} ml"
 }
 
 //@Preview(showBackground = true)
